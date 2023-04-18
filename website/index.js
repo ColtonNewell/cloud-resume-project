ece24c602231c84800256b75ed1faff7714a83ef
ColtonNewell <113714005+ColtonNewell@users.noreply.github.com>
2023-04-18 11:07:06 -0700

    Update index.js

--- website/index.js
+++ website/index.js
@@ -1,4 +1,4 @@
-fetch('https://1kf9z3nut9.execute-api.us-west-2.amazonaws.com/default/visitorcountfunction')
+fetch('https://*.execute-api.us-west-2.amazonaws.com/default/visitorcountfunction')
       .then(response => response.json())
       .then((data) => {
         document.getElementById('visitors').innerText = data.count
